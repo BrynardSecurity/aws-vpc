7fdfe6be41fa50f870a02f60d375f4364820fa5b
Ralph <ralph@thebrynards.com>
2021-11-22 10:55:11 -0600

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,3 +72,8 @@ module "vpc_endpoints_nocreate" {
   source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
   create = false
 }
+
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = module.vpc.vpc_id
+}
